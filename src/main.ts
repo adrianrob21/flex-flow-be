d05ee572c7cb780052e082a763acbd2303773bf1
Adrian <adrian@luminos.software>
2024-05-12 22:41:49 +0300

    Cors

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,14 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { NestFactory } from "@nestjs/core";
+import { AppModule } from "./app.module";
+import { CorsOptions, CorsModule } from "cors";
+
+const corsOptions: CorsOptions = {
+  origin: ["http://localhost:3000"],
+};
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.use(CorsModule.forRoot(corsOptions));
   await app.listen(3000);
 }
 bootstrap();
