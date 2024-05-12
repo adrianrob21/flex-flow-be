d563bed1ebfd2898feef90700a957f2165db58fa
Adrian <adrian@luminos.software>
2024-05-12 23:00:29 +0300

    enable cors

--- dist/main.js
+++ dist/main.js
@@ -2,7 +2,9 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 const core_1 = require("@nestjs/core");
 const app_module_1 = require("./app.module");
+const cors_1 = require("cors");
 async function bootstrap() {
+    console.log(cors_1.default);
     const app = await core_1.NestFactory.create(app_module_1.AppModule);
     await app.listen(3000);
 }
--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,9 @@
 import { NestFactory } from "@nestjs/core";
 import { AppModule } from "./app.module";
-import { CorsOptions, CorsModule } from "cors";
-
-const corsOptions: CorsOptions = {
-  origin: ["http://localhost:3000"],
-};
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.use(CorsModule.forRoot(corsOptions));
+  app.enableCors();
   await app.listen(3000);
 }
 bootstrap();
